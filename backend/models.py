ac531eb69fc1f744693a83b5af424d9120966ac0
olgashulga-lev <rubyshulga@gmail.com>
2026-07-17 13:54:11 +0300

    Регистрация

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, timedelta
+from datetime import datetime
 
 Base = declarative_base()
 
--- telegram/models.py
+++ telegram/models.py
@@ -1,7 +1,1 @@
-from sqlalchemy import Column, Integer, String, DateTime, Text
-from sqlalchemy.ext.declarative import declarative_base
-from datetime import datetime, timedelta
-
-Base = declarative_base()
-
 class Player:
